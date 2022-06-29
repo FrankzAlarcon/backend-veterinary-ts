59007de9472ae5d1265e6a315350c82a57f2553e
Frankz <leninalarcon7@gmail.com>
2022-06-29 11:56:38 -0500

    customer service and controller finished

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import setRoutes from './src/routes';
-import { boomErrorHandler, errorHandler, logError } from './src/middlewares/errorHandler';
+import { boomErrorHandler, errorHandler, logError, prismaErrorHandler } from './src/middlewares/errorHandler';
+import { config } from 'dotenv';
 
+config();
 const app = express();
 const PORT = process.env.PORT ?? 3100;
 
@@ -12,6 +14,7 @@ app.use(express.json());
 setRoutes(app);
 
 app.use(logError);
+app.use(prismaErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
