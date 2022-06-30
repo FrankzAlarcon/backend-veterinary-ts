dd64db24989f73adbbacce684ed44bba6e2113fe
Frankz <leninalarcon7@gmail.com>
2022-06-30 16:21:36 -0500

    Allow cors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import boom from '@hapi/boom';
 import setRoutes from './src/routes';
 import { boomErrorHandler, errorHandler, jwtErrorHandler, logError, prismaErrorHandler } from './src/middlewares/errorHandler';
 import { config } from 'dotenv';
@@ -8,7 +9,19 @@ config();
 const app = express();
 const PORT = process.env.PORT ?? 3100;
 
-app.use(cors());
+const whiteList = [process.env.FRONTEND_URL]
+
+const corsOptions = {
+  origin(origin: string | undefined, next: any) {
+    if(whiteList.includes(origin) || !origin) {
+      next(null, true);
+    } else {
+      next(boom.unauthorized('Cors Error'));
+    }
+  } 
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 setRoutes(app);
